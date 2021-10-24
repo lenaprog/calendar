175302451de4af5d7d14ea44d2e2b14d3e354ca8
lenaprog <lenaschaerer@web.de>
2021-10-24 23:02:35 +0200

    little changes in HTMLtry to add event

--- main.js
+++ main.js
@@ -1,29 +1,62 @@
-//when current date equals number in calendar = Highligt number
+//when current date equals number in calendar = Highlight number
 
 console.log("Hello")
 
 const months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
 
-let currentMonth = "April"
+
+const d = new Date();
+let currentMonth = d.getMonth()
+console.log(currentMonth)
 
 function getCurrentMonth () {
     for (let x=0; x<months.length; x++){
-            if (months[x]==currentMonth){
+            if (x==currentMonth){
                 return(months[x]);
-            }
-            
-        }      
+        }
+    }      
 }
-getCurrentMonth()
+
 
 function getNextMonth (){
     for (let x=0; x<months.length; x++){
-        if (months[x]==currentMonth){
+        if (x==currentMonth){
             console.log("Hi")
-            console.log(months[x+1]);
-            
-        }
-        
+            return (months[x+1]);
+   
+        }   
     }      
 }
+
 getNextMonth()
+
+
+function getPreviousMonth (){
+    for (let x=0; x<months.length; x++){
+        if (months[x]==currentMonth){
+            console.log("Yes")
+            return (months[x-1]);
+        }   
+    }      
+}
+getPreviousMonth();
+
+const dates = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+
+
+function getCurrentDate () {
+let currentDate = d.getDate()
+return currentDate;
+}
+
+function highlightDay() {
+    const today=document.getElementById(24)
+
+}
+
+
+//document.getElementById("next").onclick = getNextMonth;
+
+//document.getElementById("prev").onclick = getPreviousMonth;
+
+
